7271c6246aedd09741b6ee3a89ba2e30e2239f59
Vladyslav Svitana <vladik719@gmail.com>
2022-05-05 20:12:43 +0200

    default_files_for_header_script

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import './sass/main.scss';
 import API from './scripts/services/API';
 import renderMainPage from './scripts/renderMainPage';
 import { scrollToTop } from './scripts/scroll-to-top';
+import header from './scripts/header';
