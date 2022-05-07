e524a383f3a16be7f2b9c1cdf0ee2b118f6649c7
Igor Panama <ipanama.socialnetwork@gmail.com>
2022-05-07 19:26:33 +0300

    update dark-theme

--- src/scripts/dark-theme.js
+++ src/scripts/dark-theme.js
@@ -1,15 +1,19 @@
 const themeBtnEl = document.querySelector('.header-theme');
 const scrollTopBtnEl = document.querySelector('.scroll-top');
+const headerEl = document.querySelector('.header');
 const footerEl = document.querySelector('.footer');
 const modalEl = document.querySelector('.modal');
 const modalCloseBtnEl = document.querySelector('.modal-close-btn');
+const backDropModalEl = document.querySelector('.backdrop');
 
 if (localStorage.getItem('style') == 'dark') {
   document.body.classList.toggle('dark');
+  headerEl.classList.toggle('dark-header');
   footerEl.classList.toggle('dark-footer');
   scrollTopBtnEl.classList.toggle('dark-scroll-top');
   modalEl.classList.toggle('dark-modal');
   modalCloseBtnEl.classList.toggle('dark-modalCloseBtn');
+  backDropModalEl.classList.toggle('dark-modalBackdrop');
 }
 
 themeBtnEl.addEventListener('click', changeTheme);
@@ -27,8 +31,8 @@ function changeTheme() {
 function toggleClass() {
   footerEl.classList.toggle('dark-footer');
   scrollTopBtnEl.classList.toggle('dark-scroll-top');
+  headerEl.classList.toggle('dark-header');
   modalEl.classList.toggle('dark-modal');
   modalCloseBtnEl.classList.toggle('dark-modalCloseBtn');
+  backDropModalEl.classList.toggle('dark-modalBackdrop');
 }
-
-console.log(modalEl.children);
